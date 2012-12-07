b6dc57cc6ea27882c7f2e37eee72afcc703e09b8
Raymond-Wang <rswang@ualberta.ca>
2012-12-06 20:26:43 -0700

    changes to port and models

--- init.rb
+++ init.rb
@@ -2,5 +2,5 @@ require 'rubygems'
 require 'data_mapper'
 
 DataMapper::Logger.new($stdout, :debug)
-DataMapper.setup(:default,'mysql://group4:group4_M547f3es@posterevent.com/group4')
+DataMapper.setup(:default, 'mysql://group4:YQupPp9E@mysqlsrv.ece.ualberta.ca:13010/group4')
 
